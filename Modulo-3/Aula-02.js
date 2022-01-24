361f8d82c4a2a119ef6106a2857700d54699a631
jhenriqueb12 <henrique.barros.hb@outlook.com>
2022-01-23 21:34:01 -0300

    Aulas de CSS finalizadas e inicio do javascript

---
+++ Modulo-3/Aula-02.js
@@ -0,0 +1,6 @@
+/*
+O que é o Javascript?
+É onde as coisas criam inteligência
+
+Javascript é uma linguagem de programação de alto nível criada, a princípio, para ser executada em navegadores e manipular componentes de páginas web.
+*/
